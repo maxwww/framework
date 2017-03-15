4853cc451fa208e998784ba6970c0441271d3dbe
Maksim Soroka <ms.mikro.1@gmail.com>
2017-03-15 12:19:22 +0200

    Implement Application class

--- src/Application.php
+++ src/Application.php
@@ -2,6 +2,11 @@
 
 namespace Msoroka\Framework;
 
+use Msoroka\Framework\Request\Request;
+use Msoroka\Framework\Router\Exception\RouteNotFoundException;
+use Msoroka\Framework\Router\Router;
+use Msoroka\Framework\Response\Response;
+
 /**
  * Class Application
  * Msoroka Framework application instance class
@@ -13,7 +18,7 @@ class Application
     /**
      * @var array App config
      */
-    public $config =[];
+    public $config = [];
 
     /**
      * Application constructor.
@@ -29,7 +34,34 @@ class Application
      */
     public function run()
     {
-        echo "Application->run()";
+        $router = new Router($this->config['routes']);
+
+        try {
+            $route = $router->getRoute(Request::getRequest());
+
+            $route_controller = $route->getController();
+            $route_method = $route->getMethod();
+            if (class_exists($route_controller)) {
+                $reflectionClass = new \ReflectionClass($route_controller);
+                if ($reflectionClass->hasMethod($route_method)) {
+                    $controller = $reflectionClass->newInstance();
+                    $reflectionMethod = $reflectionClass->getMethod($route_method);
+                    $response = $reflectionMethod->invokeArgs($controller, $route->getParams());
+                    if ($response instanceof Response) {
+                        $response->send();
+                    }
+                }
+            }
+        } catch (RouteNotFoundException $e) {
+            echo "Route was not found";
+        } catch (\Exception $e) {
+            echo "Smth went wrong...";
+        }
+    }
+
+    public function __destruct()
+    {
+        // TODO: Implement __destruct() method.
     }
 
 }
